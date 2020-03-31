2c99cc7dff49b1e2a2812121288db37cae63e96b
nikarichardson <nika.richardson@yahoo.com>
2020-03-30 21:54:29 -0700

    create max_finder

---
+++ assignment_1/max_finder.m
@@ -0,0 +1,11 @@
+% max finder <--- DESC
+% returns the maximum value of a vector
+close all; clear; clc;
+r1 = rand(400,1);
+% Suppose you have an array or matrix with numbers. 
+% Find the maximum value in that array and which element
+% has the maximum value. How would you do that? 
+% Now do it the hard way, using for loops. 
+% This is assignment 1 of the first homework set. 
+% You can generate arrays by using rand( 10 ), magic( 4 )
+% or any other way you feel like.
